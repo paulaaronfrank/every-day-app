1758a05ba831cd10e29ffa8719aff73db76d5def
paulaaronfrank <paulaaronfrank@gmail.com>
2024-02-08 22:15:20 +0100

    simple non persistent checklist

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,55 +2,100 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Every Day</ion-title>
+        <ion-buttons slot="end">
+          <ion-button @click="showAddItemPrompt()">
+            <ion-icon slot="icon-only" :icon="addOutline"></ion-icon>
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <ion-list>
+        <ion-item v-for="(item, index) in items" :key="index">
+          <!-- Apply 'crossed-out' class conditionally -->
+          <ion-label :class="{ 'crossed-out': item.checked }">{{
+            item.name
+          }}</ion-label>
+          <ion-checkbox
+            slot="start"
+            :value="item.checked"
+            @ionChange="toggleItem(index)"
+          />
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { ref } from "vue";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonCheckbox,
+  IonButtons,
+  IonButton,
+  IonIcon,
+  alertController,
+} from "@ionic/vue";
+import { addOutline } from "ionicons/icons";
+
+interface Item {
+  name: string;
+  checked: boolean;
+}
+
+const items = ref<Item[]>([]);
+
+const showAddItemPrompt = async () => {
+  const alert = await alertController.create({
+    header: "Add New Item",
+    inputs: [
+      {
+        name: "name",
+        type: "text",
+        placeholder: "Item name",
+      },
+    ],
+    buttons: [
+      {
+        text: "Cancel",
+        role: "cancel",
+      },
+      {
+        text: "Add",
+        handler: (data) => {
+          if (data.name) {
+            // Ensure the name is not empty
+            items.value.push({ name: data.name, checked: false });
+          }
+        },
+      },
+    ],
+  });
+
+  await alert.present();
+};
+
+// Updated toggleItem to use Vue's reactivity system more effectively
+const toggleItem = (index: number) => {
+  items.value[index] = {
+    ...items.value[index],
+    checked: !items.value[index].checked,
+  };
+};
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+.crossed-out {
+  text-decoration: line-through;
 }
 </style>
